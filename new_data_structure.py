c68a0d74c4eb0890630bb85fd929c53e2b9e34ea
Anatoliy <staheev.a.n@yandex.ru>
2023-02-02 10:16:53 +0600

    добавил коментарии

--- new_data_structure.py
+++ new_data_structure.py
@@ -8,17 +8,21 @@ class NewDataStructure:
 
     def __str__(self):
         return f"|{self.student},{self.subject}:[{','.join(self.score)}]|"
-
+    
+    # присвоение новых значений атрибутам student и subject
     def rename(self, student='student name', subject='subject name'):
         self.student = student
         self.subject = subject
-
+    
+    # добавление данных в атрибут score
     def addat(self, data: str):
         self.score.append(data)
-
+    
+    # конвертация NewDataStructure в строку
     def conversToStr(self):
         return self.__str__()
-
+    
+    # конвертация строки в NewDataStructure
     def conversToNDS(self, data: str):
         data = data.strip('|').split(':')
         data[0] = data[0].split(',')
