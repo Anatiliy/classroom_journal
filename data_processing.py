c6e027d9b855f2a05612cbb5cf0c4fd60ce6c2c5
Anatoliy <staheev.a.n@yandex.ru>
2023-02-02 10:27:54 +0600

    внёс изменения в методы, написал коментарии

--- data_processing.py
+++ data_processing.py
@@ -11,57 +11,46 @@ class DataProcessing:
         
     def __str__(self):
         return self.name
-
+    
+    # присвоение атрибуту name нового значения
     def giveName(self, name: str):
         self.name = name
-
+    
+    # добавление иформации в атрибут data
     def newEntry(self, student: str, subject: str):
         self.data.append(nds(student, subject))
-        #self.data[-1] = nds(student, subject)
-
+        
+    # добавление оценки ученику
     def putScore(self,index, data: str):
         self.data[index].score.append(data)
 
-    #def toStr(self):
-        #return f"{self.name}*{';'.join(map(lambda item: item.conversToStr(), self.data))}"
-
-
-    #def toData(self, text):
-        #data = text.split('*')
-        #self.name = data[0]
-        #for item in data[1].split(';'):
-            #self.data.append(nds())
-            #self.data[-1].conversToNDS(item)
-
-    # запись информации в файл
+    # сохранение экземпляра класса в файл
     def save(self):
         if self.data:
-            data = [self.name].extend(list(map(lambda item: item.conversToStr(), self.data)))
+            data = [self.name] + list(map(lambda item: item.conversToStr(), self.data))
             with open(self.path, 'w', encoding='utf-8') as output_file:
-                output_file.writelines(data)
+                print(*data, sep='\n', file=output_file)
 
         else:
             with open(self.path, 'w', encoding='utf-8') as output_file:
                 output_file.write('')
 
-
     # считывание всей информации из файла
     def upload(self):
         with open(self.path, 'r', encoding='utf-8') as input_file:
-            data = input_file.readlines()
+            data = list(map(lambda item: item.rstrip(), input_file.readlines()))
         if data:
+            self.name = data.pop(0)
             for item in data:
                 self.data.append(nds())
                 self.data[-1].conversToNDS(item)
-                
 
 # перезапись файла информацией из переменной "data"
 def overwrite(data):
     if data:
         with open('journal_list.csv', 'w', encoding='utf-8') as output_file:
             print(*data, sep='\n', file=output_file)
-            #output_file.writelines(list(map(lambda item: ';'.join(item) + '\n', data)))
-
+            
 # считывание информации из файла
 def read_data():
     if os.path.exists('journal_list.csv'):
@@ -71,16 +60,3 @@ def read_data():
     else:
         return []
 
-
-# поиск информации в файле по ключу "key" с последующей записью в переменную "result_data"
-#def find(key):
-    #with open('telephone_directory.csv', 'r', encoding='utf-8') as input_file:
-        #data = input_file.readline()
-        #result_data = []
-        #while data != '':
-            #data = data.strip().split(';')
-            #if key.lower() in list(map(lambda item: item.lower(), data)):
-                #result_data.append(data)
-            #data = input_file.readline()
-    #return result_data
-    
